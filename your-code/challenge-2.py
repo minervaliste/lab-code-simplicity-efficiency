e8488e4c88dd4b5873ee0ec87cccec6f5183ef9f
Minerva Liste <minerva.liste@gmail.com>
2020-02-02 23:52:44 +0100

    challenges 1 and 2 completed

--- your-code/challenge-2.py
+++ your-code/challenge-2.py
@@ -1,38 +1,39 @@
-"""
-The code below generates a given number of random strings that consists of numbers and 
-lower case English letters. You can also define the range of the variable lengths of
-the strings being generated.
+import random
+import string
+'''
+Create a function that controls all the initial dialogue with the user. The control of the correct input
+is integrated in this function too, to avoid a "Try again" message that makes the user launch the program
+again every time.
+'''
+def request_code():
+    a = int(input('Enter minimum string length: '))
+    b = int(input('Enter maximum string length: '))
+    while b < a:
+        print('Maximum string length should be higher than minimum string length')
+        b = int(input('Enter maximum string length: '))
+    n = int(input('How many random strings to generate? '))
+    return a, b, n
 
-The code is functional but has a lot of room for improvement. Use what you have learned
-about simple and efficient code, refactor the code.
-"""
-
-def RandomStringGenerator(l=12, a=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9']):
-    p = 0
-    s = ''
-    while p<l:
-        import random
-        s += random.choice(a)
-        p += 1
-    return s
-
-def BatchStringGenerator(n, a=8, b=12):
+'''
+Create a function that generates as many codes as requested. Since the values are already correct,
+no validation is included here. Moreover, randint is used independently of a and b values (if equal, the
+same number is passed to 'generate_random_string' function all the time) 
+'''
+def create_codes(a=8, b=12, n=1):
     r = []
     for i in range(n):
-        c = None
-        if a < b:
-            import random
-            c = random.choice(range(a, b))
-        elif a == b:
-            c = a
-        else:
-            import sys
-            sys.exit('Incorrect min and max string lengths. Try again.')
-        r.append(RandomStringGenerator(c))
+        c = random.randint(a, b)
+        r.append(generate_random_string(c))
     return r
+'''
+Define a function that generates a random string of lowercase letters and digits for a given length. The
+string library is used to avoid creating a list with all the possilble vallues.
+'''
+def generate_random_string(length=12):
+    possible_values = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(possible_values) for i in range(length))
 
-a = input('Enter minimum string length: ')
-b = input('Enter maximum string length: ')
-n = input('How many random strings to generate? ')
+if __name__ == '__main__':
 
-print(BatchStringGenerator(int(n), int(a), int(b)))
+    a, b, n = request_code()
+    print('Here you are!', create_codes(a, b, n))
