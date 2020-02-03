d382ed26f78c1fa5877212e30b44d138a69702ee
Minerva Liste <minerva.liste@gmail.com>
2020-02-03 19:01:27 +0100

    add challenge 3 complete

--- your-code/challenge-3.py
+++ your-code/challenge-3.py
@@ -11,20 +11,19 @@ For example, if you are given the number 15, there are 3 possibilities to compos
 The following function shows one way to solve the problem but the code is not ideal or efficient.
 Refactor the code based on what you have learned about code simplicity and efficiency.
 """
+'''
+Start checking from the top value, so we don't have to finish the loop in order to ensure we reached the highest possible value.
+Since we know that the hipotenuse is always going to be the largest side of the 3, once we find the  correct combination we can just return 
+that number.
+'''
+def search_largest_side(max_num):
+    for x in range(max_num, 5, -1):
+        for y in range( max_num, 4,-1):
+            for z in range(max_num, 3, -1):
+                if (x*x == y*y+z*z):
+                    return x
 
-def my_function(X):
-    solutions = []
-    for x in range(5, X):
-        for y in range(4, X):
-            for z in range(3, X):
-                if (x*x==y*y+z*z):
-                    solutions.append([x, y, z])
-    m = 0
-    for solution in solutions:
-        if m < max(solution):
-            m = max(solution)
-    return m
 
-X = input("What is the maximal length of the triangle side? Enter a number: ")
-
-print("The longest side possible is " + str(my_function(int(X))))
+max_num = int(input('What is the maximal length of the triangle side? Enter a number: '))
+max_side = search_largest_side(max_num)
+print('The longest side possible is ', max_side)
